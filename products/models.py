a1f682018a4f9d4497e12ad0031cf07d91dc69bd
IGassan <igasan96@gmail.com>
2023-04-20 13:10:51 +0300

    Работа с Django ORM

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+# для того чтобы указать что эти классы будут затем использоваться для создание таблиц БД
+# с параметрах указывается models.Model
+class ProductCategory(models.Model):
+    # так как мы используем эти поля для создания БД(а в них требуется указывать тип данных)
+    # мы через класс models указываем тип данных и его параметры
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
