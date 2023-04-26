c04adb60fba8db20d9f21b775143e808c67e95dd
IGassan <igasan96@gmail.com>
2023-04-26 07:34:30 +0300

    Продолжение работы с Django ORM

--- store/settings.py
+++ store/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = (
         BASE_DIR / 'static',                        # прикручиваем статику которую отдельно загрузили в папку static
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
